f2198ad7cb72b26bd9a98c85e68e987ea4e35c73
AT Java <bernardo.baims@gmail.com>
2025-05-05 08:53:09 -0300

    commit

---
+++ ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        public delegate decimal CalculateDiscount(decimal precoOriginal);
+
+        public static decimal BotarDesconto(decimal preco)
+        {
+            return preco * 0.9m;
+        }
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço original do produto:");
+            decimal preco = Convert.ToDecimal(Console.ReadLine());
+
+            CalculateDiscount desconto = BotarDesconto;
+
+            Console.WriteLine($"Preço com desconto: R${desconto(preco)}");
+        }
+    }
+}
---
+++ ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ex3
+{
+    internal class Program
+    {
+        public delegate void Ponteiro(double result);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a altura do retangulo:");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a base do retangulo:");
+            double baseRet = double.Parse(Console.ReadLine());
+
+            Func <double, double, double> calcArea = (altura, baseRet) => (altura * baseRet);
+
+            double areaRet = calcArea(altura, baseRet);
+
+            Ponteiro ponteiro = result => Console.WriteLine($"A area do retangulo é {result}");
+            ponteiro(areaRet);
+        }
+
+    }
+}
